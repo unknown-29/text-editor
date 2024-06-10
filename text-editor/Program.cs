e3dd5c979ce144e3ad1efddff8be0b04073125d9
unknown-29 <devgmehta4@gmail.com>
2024-06-11 03:19:03 +0530

    dockerized

--- text-editor/Program.cs
+++ text-editor/Program.cs
@@ -5,9 +5,14 @@ using text_editor.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionStringTmp = builder.Configuration.GetConnectionString("DefaultConnection");
+connectionStringTmp= connectionStringTmp.Replace("SERVER_ENV", Environment.GetEnvironmentVariable("Server"));
+connectionStringTmp = connectionStringTmp.Replace("DATABASE_ENV", Environment.GetEnvironmentVariable("Database"));
+connectionStringTmp = connectionStringTmp.Replace("USER_ENV", Environment.GetEnvironmentVariable("User"));
+connectionStringTmp = connectionStringTmp.Replace("PASSWORD_ENV", Environment.GetEnvironmentVariable("Password"));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseMySql(connectionStringTmp, ServerVersion.AutoDetect(connectionStringTmp)));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -15,7 +20,13 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
+// Apply database migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
